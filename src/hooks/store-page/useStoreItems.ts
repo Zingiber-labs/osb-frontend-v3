3b8117a45187baec3d547288abc4d150276c98e8
PaoAguilar <pao.aquev29@gmail.com>
2025-10-12 12:01:14 -0600

    🐛 remove unused imports

--- src/hooks/store-page/useStoreItems.ts
+++ src/hooks/store-page/useStoreItems.ts
@@ -1,10 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { useAuth } from "@/contexts/AuthContext";
 import { api } from "@/lib/axios";
 import { ItemType } from "@/types/inventory-items";
 
 export const useStoreItems = ({ type }: { type?: ItemType } = {}) => {
-  const { isAuthenticated, isLoading: authLoading } = useAuth();
 
   return useQuery({
     queryKey: ["store-items", { type }],
